aea0b4e95675a3fc9d50d5e0dfb7efde302a5c9d
Gurkeerat Singh <gurkeerat.singh.96@gmail.com>
2017-11-13 19:26:43 +0100

    Examen MQ Client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {LoginFormComponent} from './components/login-form/login-form.component';
+import {StudentListComponent} from './components/student-list/student-list.component';
+import {SubjectListComponent} from './components/subject-list/subject-list.component';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, LoginFormComponent, StudentListComponent, SubjectListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
